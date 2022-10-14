38480c1c615982e36b212ab98eb3b3bb52b974bd
abela007 <abelgoytom77@gmail.com>
2022-10-13 17:39:45 -0700

    last task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(viod)
+{
+	printf("size of a char: %i byte(s)\n", sizeof(char));
+	printf("size of an int: %i byte(s)\n", sizeof(int));
+        printf("size of a long int: %i byte(s)\n", sizeof(long int));
+        printf("size of a long long int: %i byte(s)\n", sizeof(long long int));
+       printf("size of a float: %i byte(s)\n", sizeof(float));
+       return(0);
+}       
